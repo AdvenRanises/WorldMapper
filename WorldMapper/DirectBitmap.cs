81e57aacbd3df71a4c1cb77dedf735dfd3a3929a
AdvenRanises <110085960+AdvenRanises@users.noreply.github.com>
2026-06-21 02:41:45 +0800

    Update DirectBitmap.cs

--- WorldMapper/DirectBitmap.cs
+++ WorldMapper/DirectBitmap.cs
@@ -1,54 +1,37 @@
-// wooooo stealing code https://stackoverflow.com/questions/24701703/c-sharp-faster-alternatives-to-setpixel-and-getpixel-for-bitmaps-for-windows-f
 using System;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.Runtime.InteropServices;
+using System.IO;
+using SkiaSharp;
 
 namespace WorldMapper
 {
     public class DirectBitmap : IDisposable
     {
-        public Bitmap Bitmap { get; private set; }
-        public Int32[] Bits { get; private set; }
-        public bool Disposed { get; private set; }
+        public SKBitmap Bitmap { get; private set; }
         public int Height { get; private set; }
         public int Width { get; private set; }
-
-        protected GCHandle BitsHandle { get; private set; }
 
         public DirectBitmap(int width, int height)
         {
             Width = width;
             Height = height;
-            Bits = new Int32[width * height];
-            BitsHandle = GCHandle.Alloc(Bits, GCHandleType.Pinned);
-            Bitmap = new Bitmap(width, height, width * 4, PixelFormat.Format32bppPArgb,
-                BitsHandle.AddrOfPinnedObject());
+            Bitmap = new SKBitmap(width, height, SKColorType.Rgba8888, SKAlphaType.Premul);
         }
 
-        public void SetPixel(int x, int y, Color colour)
+        public void SetPixel(int x, int y, SKColor colour)
         {
-            int index = x + (y * Width);
-            int col = colour.ToArgb();
-
-            Bits[index] = col;
+            Bitmap.SetPixel(x, y, colour);
         }
 
-        public Color GetPixel(int x, int y)
+        public void Save(string path)
         {
-            int index = x + (y * Width);
-            int col = Bits[index];
-            Color result = Color.FromArgb(col);
-
-            return result;
+            using var data = Bitmap.Encode(SKEncodedImageFormat.Png, 100);
+            using var stream = File.OpenWrite(path);
+            data.SaveTo(stream);
         }
 
         public void Dispose()
         {
-            if (Disposed) return;
-            Disposed = true;
-            Bitmap.Dispose();
-            BitsHandle.Free();
+            Bitmap?.Dispose();
         }
     }
 }
